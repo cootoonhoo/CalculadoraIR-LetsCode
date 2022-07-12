ae76c1737a82618faacc5495417972f6aa1d06c9
Marco Antonio <cootoonhoo@gmail.com>
2022-07-12 09:30:11 -0300

    Criação das repartições + Configurando Injeção de Dependência

---
+++ CalculadoraIR/CalculadoraIR/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculadoraIR
+{
+    public class Program
+    {
+        public void Main()
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var serviceProvicer = services.BuildServiceProvider();
+
+
+        }
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
